df148a88091aa88001b168e3f6ba3bedbf7d7afa
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-06-02 14:19:10 +0900

    Todo.go UpdateTodo

--- todo_app/main.go
+++ todo_app/main.go
@@ -65,9 +65,15 @@ func main() {
 		}
 	*/
 
-	user3, _ := models.GetUser(3)
-	todos, _ := user3.GetTodosByUser()
-	for _, v := range todos {
-		fmt.Println(v)
-	}
+	/*
+		user3, _ := models.GetUser(3)
+		todos, _ := user3.GetTodosByUser()
+		for _, v := range todos {
+			fmt.Println(v)
+		}
+	*/
+
+	t, _ := models.GetTodo(1)
+	t.Content = "Update Todo"
+	t.UpdateTodo()
 }
