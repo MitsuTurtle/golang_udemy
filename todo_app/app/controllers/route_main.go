81ac45e5a616feb4093933e8e78fe44c64bdc0a6
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-06-09 10:06:33 +0900

    route_auth.go Logout1

--- todo_app/app/controllers/route_main.go
+++ todo_app/app/controllers/route_main.go
@@ -5,10 +5,20 @@ import (
 )
 
 func top(w http.ResponseWriter, r *http.Request) {
-	// t, err := template.ParseFiles("app/views/templates/top.html")
-	// if err != nil {
-	// 	log.Fatalln(err)
-	// }
-	// t.Execute(w, "Hello")
-	generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	_, err := session(w, r)
+	if err != nil {
+		generateHTML(w, "Hello", "layout", "public_navbar", "top")
+	} else {
+		http.Redirect(w, r, "/todos", 302)
+	}
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	_, err := session(w, r)
+	if err != nil {
+		http.Redirect(w, r, "/", 302)
+	} else {
+		generateHTML(w, nil, "layout", "private_navbar", "index")
+	}
+
 }
