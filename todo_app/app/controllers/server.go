aabf8e9973f7c47b8df104a75e8e0b9c8091f3c5
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-06-09 11:43:05 +0900

    route_auth.go Logout2

--- todo_app/app/controllers/server.go
+++ todo_app/app/controllers/server.go
@@ -20,10 +20,10 @@ func generateHTML(w http.ResponseWriter, data interface{}, filenames ...string) 
 
 func session(w http.ResponseWriter, r *http.Request) (sess models.Session, err error) {
 	cookie, err := r.Cookie("_cookie")
-	if err != nil {
+	if err == nil {
 		sess = models.Session{UUID: cookie.Value}
 		if ok, _ := sess.CheckSession(); !ok {
-			err = fmt.Errorf("Invalid session")
+			err = fmt.Errorf("invalid session")
 		}
 	}
 	return sess, err
@@ -37,6 +37,7 @@ func StartMainServer() error {
 	http.HandleFunc("/signup", signup)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/authenticate", authenticate)
+	http.HandleFunc("/logout", logout)
 	http.HandleFunc("/todos", index)
 	return http.ListenAndServe(":"+config.Config.Port, nil)
 }
