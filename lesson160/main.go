75f0314893147edb7dcd031c008af7290b65fb70
Mitsuhiro Kawashima <mshimashima@gmail.com>
2024-06-09 16:52:24 +0900

    Generics vector

---
+++ lesson160/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+type Vector[T any] []T
+
+type IntVector = Vector[int]
+
+func main() {
+	var v Vector[int] = Vector[int]{10, 20, 30}
+
+	fmt.Println(v)
+
+	var v2 Vector[float64] = Vector[float64]{1.3, 3.4, 5.6}
+
+	fmt.Println(v2)
+
+	// var v3 IntVector = IntVector{10, 20, 30}
+	v3 := IntVector{1, 2, 3}
+	fmt.Println(v3)
+}
